d518598ae9f1728e4e3763195579b4cfb010f996
Daniel <Daniel.Carrera@nbcuni.com>
2024-08-28 04:52:58 -0500

    refactor: Update file upload logic and document links in inf-step-three and invoice-juridica-form components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,37 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { FormsCmoComponent } from './pages/forms-cmo/forms-cmo.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { authGuard } from './guards/auth.guard';
-import { ThanksComponent } from './components/organisms/thanks/thanks.component';
 import { InvoiceLodgingComponent } from './pages/invoice-lodging/invoice-lodging.component';
 import { SendOcComponent } from './pages/send-oc/send-oc.component';
 import { ValidateOcInfoComponent } from './pages/validate-oc-info/validate-oc-info.component';
 import { OcInfoErrorComponent } from './pages/oc-info-error/oc-info-error.component';
 import { authoOcGuard } from './guards/auth-oc.guard';
-import { InvoiceNaturalFormComponent } from './components/organisms/invoice-natural-form/invoice-natural-form.component';
-import { InvoiceJuridicaFormComponent } from './components/organisms/invoice-juridica-form/invoice-juridica-form.component';
 import { OcFormsCmoComponent } from './pages/oc-forms-cmo/oc-forms-cmo.component';
 import { OcFormSuccessComponent } from './components/oc-form-success/oc-form-success.component';
 
 export const routes: Routes = [
-    {
-        path: 'home/:id',
-        component: HomeComponent
-    },
-    {
-      path: 'complete-form/:id',
-      canActivate: [authGuard],
-      component: FormsCmoComponent
-    },
-    {
-      path: 'thanks-docs/:id',
-      canActivate: [authGuard],
-      component: ThanksComponent,
-      data: {
-          type: 'docs'
-      }
-    },
     {
       path: 'facturascmo',
       component: InvoiceLodgingComponent,
