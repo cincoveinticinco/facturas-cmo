3158246bcda63bc495270e1e5fec5819bf25b51c
Josego525 <93600613+Josego525@users.noreply.github.com>
2025-08-27 12:27:58 -0500

    feat: Development enviroment

---
+++ src/environments/environment.development-server.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://developmentapi.onewrapp.com/",
+  apiUrlFront: "http://developmentclients.onewrapp.com/radicacioncmo/",
+  stage: "development-server"
+};
